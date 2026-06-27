250b6e662b4692b5c180cd5f7ed0c4f52adae79d
achunt-6 <1327606237@qq.com>
2026-06-27 10:41:54 +0800

    Delete springboot directory

--- springboot/takeout/src/main/java/com/cpy/takeout/dao/UserAddressMapper.java
+++
@@ -1,8 +0,0 @@
-package com.cpy.takeout.dao;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cpy.takeout.entity.UserAddress;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserAddressMapper extends BaseMapper<UserAddress> {
-}
